b6044f465ea2f33dc57af4eae26b6a9b1d627dce
Bojken07 <bojken.zaimi@stud.sti.se>
2024-09-23 15:29:22 +0200

    skapa nytt projekt

---
+++ src/main/java/com/bojken/ws_projektarbete_6/WsProjektarbete6Application.java
@@ -0,0 +1,13 @@
+package com.bojken.ws_projektarbete_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProjektarbete6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsProjektarbete6Application.class, args);
+	}
+
+}

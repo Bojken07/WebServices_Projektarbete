4f4bbe8baae42a004706792e9c7c397f79e48979
Bojken07 <bojken.zaimi@stud.sti.se>
2024-09-23 15:41:19 +0200

    jobba med Movie_class

--- src/main/java/com/bojken/ws_projektarbete_6/config/WebClientConfig.java
+++ src/main/java/com/bojken/ws_projektarbete_6/config/WebClientConfig.java
@@ -1,5 +1,8 @@
 package com.bojken.ws_projektarbete_6.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class WebClientConfig {
 

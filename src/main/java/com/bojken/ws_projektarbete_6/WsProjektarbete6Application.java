1beafa7669f0de2dc0b6543dc5b3c178844009a3
Bojken07 <bojken.zaimi@stud.sti.se>
2024-11-26 20:14:38 +0100

    Add: Docker implementation

--- src/main/java/com/bojken/ws_projektarbete_6/WsProjektarbete6Application.java
+++ src/main/java/com/bojken/ws_projektarbete_6/WsProjektarbete6Application.java
@@ -2,6 +2,8 @@ package com.bojken.ws_projektarbete_6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WsProjektarbete6Application {
@@ -9,6 +11,11 @@ public class WsProjektarbete6Application {
 	public static void main(String[] args) {
 
 		SpringApplication.run(WsProjektarbete6Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
